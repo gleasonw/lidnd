732f8660b3922a8cf32ad9a2222c8975242dd05f
Will Gleason <will.gleason@novadiscovery.com>
2023-10-22 16:25:42 +0200

    latest

--- backend/main.py
+++ backend/main.py
@@ -762,4 +762,4 @@ async def untrack(ctx: ApplicationContext):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0")
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
