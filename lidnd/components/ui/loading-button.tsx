d0e69b1c1c400dc3674905d41e3b19818d16e15a
Will Gleason <wgleason9@gmail.com>
2023-11-10 10:32:54 +0100

    latest

--- lidnd/components/ui/loading-button.tsx
+++ lidnd/components/ui/loading-button.tsx
@@ -1,11 +1,9 @@
-"use client"
-
+"use client";
 
 import { Button, ButtonProps } from "@/components/ui/button";
 import { Spinner } from "@/components/ui/spinner";
 import React from "react";
 // @ts-ignore
-import { useFormStatus } from "react-dom";
 
 export interface LoadingButtonProps extends ButtonProps {
   children?: React.ReactNode;
@@ -13,12 +11,16 @@ export interface LoadingButtonProps extends ButtonProps {
 }
 
 export function LoadingButton(props: LoadingButtonProps) {
-  const { pending } = useFormStatus();
+  // const { pending } = useFormStatus();
 
-  const isPending = props.isLoading || pending;
+  // const isPending = props.isLoading || pending;
 
   const propsWithoutIsLoading = { ...props };
   delete propsWithoutIsLoading.isLoading;
 
-  return <Button {...propsWithoutIsLoading} disabled={isPending}>{isPending ? <Spinner /> : props.children}</Button>;
+  return (
+    <Button {...propsWithoutIsLoading} disabled={props.isLoading}>
+      {props.isLoading ? <Spinner /> : props.children}
+    </Button>
+  );
 }
