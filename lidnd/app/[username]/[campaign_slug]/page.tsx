d44c9ae9baf94011bafdda88bbe3419989474dc1
Will Gleason <wgleason9@gmail.com>
2025-12-27 16:07:09 -0800

    Refactor page layout by removing overflow class
    Removed 'overflow-auto' class from the main div.

--- lidnd/app/[username]/[campaign_slug]/page.tsx
+++ lidnd/app/[username]/[campaign_slug]/page.tsx
@@ -99,7 +99,7 @@ export default async function CampaignPage(props: {
   }
 
   return (
-    <div className="mx-auto flex w-full max-w-5xl flex-col gap-4 px-4 py-6 max-h-full overflow-auto">
+    <div className="mx-auto flex w-full max-w-5xl flex-col gap-4 px-4 py-6 max-h-full">
       <header className="flex flex-col gap-6 border-b pb-4">
         <div className="flex items-center justify-between gap-4">
           <Link href={appRoutes.dashboard(user)}>
