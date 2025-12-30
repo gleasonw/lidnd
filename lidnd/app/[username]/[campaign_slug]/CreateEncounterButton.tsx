0dda9c6ec368545457869ee689f784c6c2bf9f30
Will Gleason <will.gleason@centricsoftware.com>
2025-12-29 16:59:26 -0800

    hold on hotkeys

--- lidnd/app/[username]/[campaign_slug]/CreateEncounterButton.tsx
+++ lidnd/app/[username]/[campaign_slug]/CreateEncounterButton.tsx
@@ -1,21 +1,13 @@
 "use client";
 
-import {
-  useCampaign,
-  useHotkey,
-} from "@/app/[username]/[campaign_slug]/campaign-hooks";
+import { useCampaign } from "@/app/[username]/[campaign_slug]/campaign-hooks";
 import { createEncounter } from "@/app/[username]/actions";
 import { Button } from "@/components/ui/button";
-import { Kbd } from "@/components/ui/kbd";
-import { Plus } from "lucide-react";
 import { useTransition } from "react";
 
 export function CreateEncounterButton() {
   const [campaign] = useCampaign();
   const [isPending, startTransition] = useTransition();
-  useHotkey("e", () => {
-    startTransition(() => createEncounter({ campaign_id: campaign.id }));
-  });
   return (
     <form
       className="w-full flex items-center justify-center"
@@ -31,9 +23,7 @@ export function CreateEncounterButton() {
           startTransition(() => createEncounter({ campaign_id: campaign.id }))
         }
       >
-        <Plus className="mr-2 h-4 w-4" />
         Create Encounter
-        <Kbd>E</Kbd>
       </Button>
     </form>
   );
