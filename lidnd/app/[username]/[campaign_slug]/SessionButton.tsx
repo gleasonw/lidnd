1778f18937dbf88c0655c16046e5be12a239019c
Will Gleason <will.gleason@centricsoftware.com>
2026-01-02 14:33:06 -0800

    don't make session button tall

--- lidnd/app/[username]/[campaign_slug]/SessionButton.tsx
+++ lidnd/app/[username]/[campaign_slug]/SessionButton.tsx
@@ -39,7 +39,7 @@ export async function SessionButton({ campaignId }: { campaignId: string }) {
   const lastSessionData = lastSession.at(0) || null;
 
   return (
-    <div className="h-20 w-full flex">
+    <div className="w-full flex">
       {targetActiveSession ? (
         <ActiveSessionButton session={targetActiveSession} />
       ) : (
