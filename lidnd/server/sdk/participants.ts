b8f57f3a99e6bd3beec45074ee868301af8118c6
Will Gleason <wgleason9@gmail.com>
2026-04-15 17:02:43 -0700

    Keep defeated monsters in encounter with defeated styling

--- lidnd/server/sdk/participants.ts
+++ lidnd/server/sdk/participants.ts
@@ -17,24 +17,6 @@ export const ServerParticipants = {
       participant.encounter_id
     );
 
-    if (participant.hp !== undefined && participant.hp <= 0) {
-      // just remove the participant
-      const update = await dbObject
-        .delete(participants)
-        .where(eq(participants.id, participant.id))
-        .returning();
-      const updatedParticipant = update[0];
-
-      if (!updatedParticipant) {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Failed to update encounter participant",
-        });
-      }
-
-      return updatedParticipant;
-    }
-
     const update = await dbObject
       .update(participants)
       .set(participant)
